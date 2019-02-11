de545541335941d1a3cfda67013be61e1b2fcc13
Abanoub Ghadban <ghdbana@gmail.com>
2019-02-12 01:19:02 +0200

    added get join code utility

--- app/utils/codeGenerators.js
+++ app/utils/codeGenerators.js
@@ -1,8 +1,11 @@
 const ActivationCode = require('../models/ActivationCode');
+const JoinCode = require('../models/JoinCode');
+
 const config = require('config');
 const { timeAfter } = require('./utils');
 
 const activationCodeTTL = config.get('activationCodeTTL');
+const joinCodeTTL = config.get('joinCodeTTL');
 
 const generateCode = async (user, len) => {
     const possible = "abcdefghijklmnopqrstuvwxyz0123456789";
@@ -39,6 +42,32 @@ const getActivationCode = async(user, transaction) => {
     throw new Error('Failed to generate activation code');
 }
 
+// Generate Joi code and add it to db
+// Return Object of JoinCode model
+const getJoinCode = async(family, transaction) => {
+    for (let i = 0;i < 1000;++i) {
+        const code = await generateCode(user, 6);
+        const codeObj = await JoinCode.findOne({where: {code}});
+
+        if (codeObj && (codeObj.expAt > new Date()))
+            continue;
+
+        // If code already exist but expired, delete it
+        if (codeObj) {
+            await codeObj.destroy({transaction});
+        }
+        const newCode = ActivationCode.build({
+            'code': code,
+            'expAt': timeAfter(joinCodeTTL),
+            'familyId': family.id
+        });
+        await newCode.save({transaction});
+        return newCode;
+    }
+    throw new Error('Failed to generate activation code');
+}
+
 module.exports = {
-    getActivationCode
+    getActivationCode,
+    getJoinCode
 };
